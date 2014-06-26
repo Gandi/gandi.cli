614f2dcfd45c9d0d73ce53955d6f297fa593ea6c
sayoun <sayoun@gandi.net>
2014-06-26 16:59:12 +0200

    Cosmetics

--- gandi/cli/commands/global.py
+++ gandi/cli/commands/global.py
@@ -2,8 +2,8 @@
 import click
 
 from gandi.cli.core.cli import cli
-from gandi.cli.core.params import pass_gandi
 from gandi.cli.core.utils import output_generic
+from gandi.cli.core.params import pass_gandi
 
 
 @cli.command()
