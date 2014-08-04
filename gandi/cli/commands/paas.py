af056b5c033ee914177c4259edf8811a88d92525
Olivier Roussy <olivier@gandi.net>
2014-08-04 17:35:05 +0200

    Only need to init vhost when cloning.

--- gandi/cli/commands/paas.py
+++ gandi/cli/commands/paas.py
@@ -80,7 +80,7 @@ def clone(gandi, vhost):
         gandi.error('missing VHOST parameter')
 
     if vhost and not paas_access:
-        gandi.paas.init_conf(vhost, vhost=vhost)
+        gandi.paas.init_vhost(vhost, id=vhost)
     else:
         paas_access = gandi.get('paas.access')
         gandi.shell('git clone ssh+git://%s/%s.git' % (paas_access, vhost))
