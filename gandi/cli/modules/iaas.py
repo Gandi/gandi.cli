b6d66e9ccbe785c5f7780a6f7139137b5b335a94
Pascal Bouchareine <pascal@gandi.net>
2014-09-11 14:21:06 +0200

    refactor argv building in iaas.ssh()

--- gandi/cli/modules/iaas.py
+++ gandi/cli/modules/iaas.py
@@ -248,28 +248,28 @@ class Iaas(GandiModule, SshkeyHelper):
     def ssh(cls, vm_id, identity=None, wipe_key=False):
         """Spawn an ssh session to virtual machine."""
         vm_info = cls.info(vm_id)
+
+        cmd = ['ssh']
+        if identity:
+            cmd.extend(('-i', identity,))
+
         for iface in vm_info['ifaces']:
             for ip in iface['ips']:
-                if ip['version'] == 4:
-                    access = 'root@%s' % ip['ip']
-                    ip_addr = ip['ip']
-                else:
-                    access = '-6 root@%s' % ip['ip']
-                    ip_addr = ip['ip']
+                ip_addr = ip['ip']
+                if ip['version'] == 6:
+                    cmd.append('-6')
                 # stop on first access found
                 break
 
-        if identity:
-            access = '-i %s %s' % (identity, access,)
-        access = 'ssh %s' % access
+        cmd.append('root@%s' % ip_addr)
 
-        cls.echo('Requesting access using: %s ...' % access)
+        cls.echo('Requesting access using: %s ...' % ' '.join(cmd))
         # XXX: we must remove ssh key entry in case we use the same ip
         # as it's recyclable
         if wipe_key:
             cls.shell('ssh-keygen -R "%s"' % ip_addr)
 
-        cls.shell(access)
+        cls.shell(cmd, False)
 
     @classmethod
     def console(cls, id):
