feb206943820cab0e2e6dad2023eb3898949815a
Yann Brelière <yann@gandi.net>
2014-09-16 14:06:14 +0200

    Minify the generated css.
    Add a gulp task to handle this and improve the tasks a bit.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
-var watch = require('gulp-watch');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('styles', function () {
-  gulp.src('./less/main.less')
+  return gulp.src('./less/main.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less') ]
+    }).on('error', function (err) {
+      console.log(err);
     }))
     .pipe(gulp.dest('./css/'));
 });
@@ -14,3 +17,19 @@ gulp.task('styles', function () {
 gulp.task('watch', function () {
   gulp.watch('./less/*.less', ['styles']);
 });
+
+gulp.task('less', function () {
+  return gulp.src('./less/main.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less') ]
+    }).on('error', function (err) {
+      console.log(err);
+    }))
+    .pipe(cssmin().on('error', function(err) {
+      console.log(err);
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default', ['less', 'watch']);
