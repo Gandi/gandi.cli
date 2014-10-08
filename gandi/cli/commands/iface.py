5caf97a56e1681d144c5575e05823655df900356
sayoun <sayoun@gandi.net>
2014-10-08 15:17:09 +0200

    Add 'gandi iface create/delete' commands

--- gandi/cli/commands/iface.py
+++ gandi/cli/commands/iface.py
@@ -4,19 +4,16 @@ import click
 
 from gandi.cli.core.cli import cli
 from gandi.cli.core.utils import output_generic, output_iface
-from gandi.cli.core.params import pass_gandi
+from gandi.cli.core.params import option, pass_gandi, IntChoice, DATACENTER
 
 
 @cli.command()
-@click.option('--id', help='Display ids.', is_flag=True)
 @click.option('--vm', help='Display vms.', is_flag=True)
 @click.option('--vlan', help='Display vlans.', is_flag=True)
 @pass_gandi
-def list(gandi, id, ip, vm, vlan):
+def list(gandi, vm, vlan):
     """List ifaces."""
-    output_keys = ['num', 'type', 'state', 'dc', 'bandwidth']
-    if id:
-        output_keys.append('id')
+    output_keys = ['id', 'num', 'type', 'state', 'dc', 'bandwidth']
     if vm:
         output_keys.append('vm')
     if vlan:
@@ -39,7 +36,7 @@ def list(gandi, id, ip, vm, vlan):
 def info(gandi, resource):
     """Display information about a iface.
 
-    Resource can be an iface num or ID
+    Resource can be an iface ID
     """
     output_keys = ['num', 'type', 'state', 'dc', 'bandwidth', 'vm']
 
@@ -55,3 +52,70 @@ def info(gandi, resource):
         output_generic(gandi, ip, output_ips)
 
     return iface
+
+
+@cli.command()
+@option('--datacenter', type=DATACENTER, default='LU',
+        help='Datacenter where the iface will be spawned.')
+@option('--ip-version', type=IntChoice(['4', '6']), default='4',
+        help='Version of created IP.')
+@option('--bandwidth', type=click.INT, default=102400,
+        help="Network bandwidth in bit/s used to create the VM's first "
+             "network interface.")
+@click.option('--vlan', default=None, type=click.STRING,
+              help='Attach the newly created disk to the vlan.')
+@click.option('--bg', '--background', default=False, is_flag=True,
+              help='Run command in background mode (default=False).')
+@pass_gandi
+def create(gandi, ip_version, datacenter, bandwidth, vlan, background):
+    """ Create a new iface """
+    result = gandi.iface.create(ip_version, datacenter, bandwidth, vlan,
+                                background)
+
+    if not result:
+        return
+
+    if background:
+        gandi.pretty_echo(result)
+
+    return result
+
+
+@cli.command()
+@click.option('--bg', '--background', default=False, is_flag=True,
+              help='Run command in background mode (default=False).')
+@click.option('--force', '-f', is_flag=True,
+              help='This is a dangerous option that will cause CLI to continue'
+                   ' without prompting. (default=False).')
+@click.argument('resource', nargs=-1, required=True)
+@pass_gandi
+def delete(gandi, background, force, resource):
+    """Delete an iface.
+
+    Resource can be an iface ID
+    """
+    output_keys = ['id', 'type', 'step']
+
+    iface_list = gandi.iface.list()
+    iface_idlist = [iface['id'] for iface in iface_list]
+    for item in resource:
+        item_ = int(item)
+        if item_ not in iface_idlist:
+            gandi.echo('Sorry iface %d does not exist' % item_)
+            gandi.echo('Please use one of the following: %s' % iface_idlist)
+            return
+
+    if not force:
+        iface_info = "'%s'" % ', '.join(resource)
+        proceed = click.confirm('Are you sure to delete iface %s?' %
+                                iface_info)
+
+        if not proceed:
+            return
+
+    opers = gandi.iface.delete(resource, background)
+    if background:
+        for oper in opers:
+            output_generic(gandi, oper, output_keys)
+
+    return opers
--- gandi/cli/modules/iface.py
+++ gandi/cli/modules/iface.py
@@ -1,6 +1,7 @@
 """ Iface commands module. """
 
 from gandi.cli.core.base import GandiModule
+from gandi.cli.modules.datacenter import Datacenter
 
 
 class Iface(GandiModule):
@@ -29,23 +30,10 @@ class Iface(GandiModule):
         return cls._info(cls.usable_id(num))
 
     @classmethod
-    def from_num(cls, num):
-        """Retrieve iface id associated to a num."""
-        result = cls.list()
-        ifaces = {}
-        for iface in result:
-            ifaces[iface['num']] = iface['id']
-
-        return ifaces.get(int(num))
-
-    @classmethod
     def usable_id(cls, id):
         """ Retrieve id from input which can be num or id."""
         try:
-            # id is maybe a num
-            qry_id = cls.from_num(id)
-            if not qry_id:
-                qry_id = int(id)
+            qry_id = int(id)
         except Exception:
             qry_id = None
 
@@ -54,3 +42,68 @@ class Iface(GandiModule):
             cls.error(msg)
 
         return qry_id
+
+    @classmethod
+    def create(cls, ip_version, datacenter, bandwidth, vlan, background):
+        """ Create a new iface """
+        if not background and not cls.intty():
+            background = True
+
+        datacenter_id_ = int(Datacenter.usable_id(datacenter))
+
+        iface_params = {
+            'ip_version': ip_version,
+            'datacenter_id': datacenter_id_,
+            'bandwidth': bandwidth,
+        }
+        if vlan:
+            iface_params['vlan'] = vlan
+
+        result = cls.call('hosting.iface.create', iface_params)
+
+        if not background:
+            # interactive mode, run a progress bar
+            cls.echo('Creating your iface.')
+            cls.display_progress(result)
+            cls.echo('Your iface has been created.')
+
+        return result
+
+    @classmethod
+    def _detach(cls, iface_id):
+        """ Detach an iface from a vm. """
+        iface = cls._info(iface_id)
+        opers = []
+        vm_id = iface.get('vm_id')
+        if vm_id:
+            cls.echo('The iface is still attached to the vm %s.' % vm_id)
+            cls.echo('Will detach it.')
+            opers.append(cls.call('hosting.vm.disk_detach', vm_id, iface_id))
+        return opers
+
+    @classmethod
+    def delete(cls, resources, background=False):
+        """ Delete this iface."""
+        if not isinstance(resources, (list, tuple)):
+            resources = [resources]
+
+        resources = [cls.usable_id(item) for item in resources]
+
+        opers = []
+        for iface_id in resources:
+            opers.extend(cls._detach(iface_id))
+
+        if opers:
+            cls.echo('Detaching your iface(s).')
+            cls.display_progress(opers)
+
+        opers = []
+        for iface_id in resources:
+            oper = cls.call('hosting.iface.delete', iface_id)
+            opers.append(oper)
+
+        if background:
+            return opers
+
+        cls.echo('Deleting your iface.')
+        cls.display_progress(opers)
