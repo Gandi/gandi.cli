8ac17cce168c82c5162f21ea15f471d2fda01a80
Olivier Roussy <olivier@gandi.net>
2014-11-04 15:27:32 +0100

    Add a contact create in dry-run mode.

--- gandi/cli/modules/contact.py
+++ gandi/cli/modules/contact.py
@@ -16,3 +16,9 @@ class Contact(GandiModule):
     def create(cls, params):
         """Create a new contact."""
         return cls.call('contact.create', params, empty_key=True)
+
+    @classmethod
+    def create_dry_run(cls, params):
+        """Create a new contact."""
+        return cls.call('contact.create', dict(params), empty_key=True,
+                        dry_run=True, return_dry_run=True)
