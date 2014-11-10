decf8f5395e38f05cacc13084d795c585ca66dfe
Pascal Bouchareine <pascal@gandi.net>
2014-11-10 15:56:29 -0800

    refactor keyscan and ssh, add retry on failures
    Try to workaround ssh restarting during vm creation.
    This should help the initscript upload/keyscan fail less often.
    
    Improve this part until --script is uploaded to Gandi for a swapdisk
    bootstrap.

--- gandi/cli/modules/iaas.py
+++ gandi/cli/modules/iaas.py
@@ -1,6 +1,7 @@
 """ VM commands module. """
 
 import math
+import os
 import socket
 import time
 
@@ -338,7 +339,14 @@ class Iaas(GandiModule, SshkeyHelper):
         cls.echo('Wiping old key and learning the new one')
         version, ip_addr = cls.vm_ip(vm_id)
         cls.execute('ssh-keygen -R "%s"' % ip_addr)
-        cls.execute('ssh-keyscan "%s" >> ~/.ssh/known_hosts' % ip_addr)
+
+        for _ in xrange(5):
+            output = cls.exec_output('ssh-keyscan "%s"' % ip_addr)
+            if output:
+                with open(os.path.expanduser('~/.ssh/known_hosts'), 'a') as f:
+                    f.write(output)
+                return True
+            time.sleep(.5)
 
     @classmethod
     def scp(cls, vm_id, login, identity, local_file, remote_file):
@@ -354,7 +362,12 @@ class Iaas(GandiModule, SshkeyHelper):
         cmd.extend((local_file, '%s@%s:%s' %
                    (login, ip_addr, remote_file),))
         cls.echo('Running %s' % ' '.join(cmd))
-        return cls.execute(cmd, False)
+        for _ in xrange(5):
+            ret = cls.execute(cmd, False)
+            if ret:
+                break
+            time.sleep(.5)
+        return ret
 
     @classmethod
     def ssh(cls, vm_id, login, identity, args=None):
