Gandi/gandi.cli

commit fb62cdc9d8b34322c7d4891734fa8a7b9646517d

iaas: remove possible duplicates when passing multiples resources
this will avoid raising errors when an operation has already been sent
for a resource

Signed-off-by: William Dauchy <william@gandi.net>

gandi/cli/commands/disk.py
@@ -66,6 +66,7 @@ def info(gandi, resource):
     output_keys = ['name', 'state', 'size', 'type', 'id', 'dc', 'vm',
                    'profile']
 
+    resource = tuple(set(resource))
     vms = dict([(vm['id'], vm) for vm in gandi.iaas.list()])
     datacenters = gandi.datacenter.list()
 
@@ -101,6 +102,7 @@ def detach(gandi, resource, background, force):
 
     Resource can be a disk name, or ID
     """
+    resource = tuple(set(resource))
     if not force:
         proceed = click.confirm('Are you sure to detach %s?' %
                                 ', '.join(resource))
@@ -129,6 +131,7 @@ def attach(gandi, disk, vm, background, force):
     disk can be a disk name, or ID
     vm can be a vm name, or ID
     """
+    resource = tuple(set(resource))
     if not force:
         proceed = click.confirm("Are you sure to attach disk '%s' to vm '%s'" %
                                 (disk, vm))
@@ -201,6 +204,7 @@ def delete(gandi, resource, force, background):
     """ Delete a disk. """
     output_keys = ['name', 'disk_id', 'state', 'date_creation']
 
+    resource = tuple(set(resource))
     if not force:
         disk_info = "'%s'" % ', '.join(resource)
         proceed = click.confirm('Are you sure to delete disk %s?' % disk_info)

gandi/cli/commands/vm.py
@@ -50,6 +50,7 @@ def info(gandi, resource):
     output_keys = ['hostname', 'state', 'cores', 'memory', 'console',
                    'datacenter', 'ip']
 
+    resource = tuple(set(resource))
     datacenters = gandi.datacenter.list()
     ret = []
     for num, item in enumerate(resource):
@@ -78,6 +79,7 @@ def stop(gandi, background, resource):
     """
     output_keys = ['id', 'type', 'step']
 
+    resource = tuple(set(resource))
     opers = gandi.iaas.stop(resource, background)
     if background:
         for oper in opers:
@@ -98,6 +100,7 @@ def start(gandi, background, resource):
     """
     output_keys = ['id', 'type', 'step']
 
+    resource = tuple(set(resource))
     opers = gandi.iaas.start(resource, background)
     if background:
         for oper in opers:
@@ -118,6 +121,7 @@ def reboot(gandi, background, resource):
     """
     output_keys = ['id', 'type', 'step']
 
+    resource = tuple(set(resource))
     opers = gandi.iaas.reboot(resource, background)
     if background:
         for oper in opers:
@@ -141,6 +145,7 @@ def delete(gandi, background, force, resource):
     """
     output_keys = ['id', 'type', 'step']
 
+    resource = tuple(set(resource))
     possible_resources = gandi.iaas.resource_list()
     for item in resource:
         if item not in possible_resources:

commit c068d89d7350c5d2d5b41d8a538357e1c20a56da

disk: remove unexisting local variable 'resource'
this fixes a bug introduced in commit:
fb62cdc iaas: remove possible duplicates when passing multiples resources

Signed-off-by: William Dauchy <william@gandi.net>

gandi/cli/commands/disk.py
@@ -131,7 +131,6 @@ def attach(gandi, disk, vm, background, force):
     disk can be a disk name, or ID
     vm can be a vm name, or ID
     """
-    resource = tuple(set(resource))
     if not force:
         proceed = click.confirm("Are you sure to attach disk '%s' to vm '%s'" %
                                 (disk, vm))