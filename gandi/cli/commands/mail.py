Gandi/gandi.cli

commit a6577bb64c98dfbe77f4fcbdad482e1b6374a98d

Remove alias param from mail delete command as it's now done in mail update command

gandi/cli/commands/mail.py
@@ -45,6 +45,7 @@ def info(gandi, email):
 @pass_gandi
 def create(gandi, email, quota, fallback, alias):
     """Create a mailbox."""
+
     login, domain = email
     options = {}
     password = click.prompt('password', hide_input=True,
@@ -64,8 +65,6 @@ def create(gandi, email, quota, fallback, alias):
 @cli.command(options_metavar='')
 @click.option('--force', '-f', help='Force mailbox deletion.',
               is_flag=True)
-@click.option('--alias', '-a', help='Remove mailbox alias.',
-              multiple=True)
 @click.argument('email', type=EMAIL_TYPE, metavar='login@domain.tld')
 @pass_gandi
 def delete(gandi, email, force, alias):
@@ -73,20 +72,14 @@ def delete(gandi, email, force, alias):
 
     login, domain = email
 
-    if alias:
-        aliases = gandi.mail.info(domain, login)['aliases']
-        for element in alias:
-            aliases.remove(element)
-        result = gandi.mail.set_alias(domain, login, aliases)
-    else:
-        if not force:
-            proceed = click.confirm('Are you sure to delete the '
-                                    'mailbox %s@%s ?' % (login, domain))
+    if not force:
+        proceed = click.confirm('Are you sure to delete the '
+                                'mailbox %s@%s ?' % (login, domain))
 
-            if not proceed:
-                return
+        if not proceed:
+            return
 
-        result = gandi.mail.delete(domain, login)
+    result = gandi.mail.delete(domain, login)
 
     return result
 

commit 724531e3d26e7f0ad9e342ca851a5d78c0116bb2

fix regression on mail delete
commit a6577bb64c98dfbe introduced issue #64

gandi/cli/commands/mail.py
@@ -67,7 +67,7 @@ def create(gandi, email, quota, fallback, alias):
                    ' without prompting. (default=False).')
 @click.argument('email', type=EMAIL_TYPE, metavar='login@domain.tld')
 @pass_gandi
-def delete(gandi, email, force, alias):
+def delete(gandi, email, force):
     """Delete a mailbox."""
     login, domain = email
 