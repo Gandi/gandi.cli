6ebffc58acd3d15c9bf5d5eb7277b9a89e8d20e9
Guillaume Gauvrit <guillaume@gauvr.it>
2015-01-03 16:03:21 +0100

    Fix dependencies
    console_scripts is a setuptools features

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(here, 'gandi', 'cli', '__init__.py')) as v_file:
     version = re.compile(r".*__version__ = '(.*?)'",
                          re.S).match(v_file.read()).group(1)
 
-requires = ['pyyaml', 'click<=4.0', 'requests']
+requires = ['setuptools', 'pyyaml', 'click<=4.0', 'requests']
 
 
 setup(name='gandi.cli',
