c624ba391d8e8c269c97e5d58da452e6050fc874
Olivier Roussy <olivier@gandi.net>
2015-01-14 15:44:38 +0100

    Fix certificate tests.

--- gandi/cli/tests/commands/test_certificate.py
+++ gandi/cli/tests/commands/test_certificate.py
@@ -10,25 +10,30 @@ class CertTestCase(CommandTestCase):
         result = self.runner.invoke(certificate.packages, [])
 
         #self.assertEqual(result.exit_code, 0)
-        self.assertEqual(result.output, """cert_std_1_0_0
-cert_std_w_0_0
-cert_std_3_0_0
-cert_std_5_0_0
-cert_std_10_0_0
-cert_std_20_0_0
-cert_pro_1_100_0
-cert_pro_1_100_SGC
-cert_pro_1_10_0
-cert_pro_1_250_0
-cert_pro_w_250_0
-cert_pro_w_250_SGC
-cert_bus_1_250_0
-cert_bus_1_250_SGC
-cert_bus_3_250_0
-cert_bus_5_250_0
-cert_bus_10_250_0
-cert_bus_20_250_0
+        wanted = (
+"""name               | description                   | max altname | flavor
+-------------------+-------------------------------+-------------+-------
+cert_std_1_0_0     | single domain std certificate | 1           | std   
+cert_std_w_0_0     | wildcard std certificate      | 1           | std   
+cert_std_3_0_0     | multi domain std certificate  | 3           | std   
+cert_std_5_0_0     | multi domain std certificate  | 5           | std   
+cert_std_10_0_0    | multi domain std certificate  | 10          | std   
+cert_std_20_0_0    | multi domain std certificate  | 20          | std   
+cert_pro_1_100_0   | single domain pro certificate | 1           | pro   
+cert_pro_1_100_SGC | single domain pro certificate | 1           | pro   
+cert_pro_1_10_0    | single domain pro certificate | 1           | pro   
+cert_pro_1_250_0   | single domain pro certificate | 1           | pro   
+cert_pro_w_250_0   | wildcard pro certificate      | 1           | pro   
+cert_pro_w_250_SGC | wildcard pro certificate      | 1           | pro   
+cert_bus_1_250_0   | single domain bus certificate | 1           | bus   
+cert_bus_1_250_SGC | single domain bus certificate | 1           | bus   
+cert_bus_3_250_0   | multi domain bus certificate  | 3           | bus   
+cert_bus_5_250_0   | multi domain bus certificate  | 5           | bus   
+cert_bus_10_250_0  | multi domain bus certificate  | 10          | bus   
+cert_bus_20_250_0  | multi domain bus certificate  | 20          | bus   
 """)
+        self.assertEqual(result.output, wanted)
+
         self.assertEqual(result.exit_code, 0)
 
     def test_list(self):
@@ -56,12 +61,34 @@ status       : valid
         self.assertEqual(result.exit_code, 0)
 
     def test_create(self):
+        csr = '''-----BEGIN CERTIFICATE REQUEST-----
+MIICWjCCAUICAQAwFTETMBEGA1UEAwwKZG9tYWluLnRsZDCCASIwDQYJKoZIhvcN
+AQEBBQADggEPADCCAQoCggEBAKYPfDoiWuWDwJb+fZhOHA++9yYy1BbxnY729hSd
+/P12kw1HeIL5CGIhZLpJrwRQmLPTlJ0VttFaqpNm7mEISr+GMJzEWBTyD8750hbW
+bXwZBcsWi8AsOsnT+sh/cTKGlJctA346HKU3tLlZsvI4ecfnlIZk5Yefgf+78abz
+SzSV47gPDUNQvGIzP9QPE4bEFu5NjdxPg3ylaQ5cv8iiWHn4iUCRXlxxNfHmH7xE
+ysFlsD6KnKjR5eYLKBcATeqopGPi72KlcDn5lmtdWsd9aGSl5KlkKQC497buqjbr
+H31lMAGAC7At6S7AF5GIT5KGjN6KyPrzUOn7FrhNUcnpUQMCAwEAAaAAMA0GCSqG
+SIb3DQEBCwUAA4IBAQCBM6wc9DfsI1htRhAz7/RfOIn7kb6LygOSEgfb757My+60
+N/WP9ndpmob0PW18B1vXBloZEkO/aNTXCGAIPJXRkeTYVhEE2B7K3pc9IiNmLxXC
+3b2cwUjgmNw9wmFZ4AuHqzWHevqix3m7Acpkl5ugcCsTVOX3mx84MSguSC+5AWfm
+DG0VmOWZ0tWjyZuKgtoXgHnH3whEac+pM7M3J+z94/msO9hnpUOQNt4XALEoONrv
++xE1FDGhRJAx9AYOtTBQSFLqKB4D6W2hhDVLirxQuJ/lC/l8tyEu96ggfDRrMXE4
+v0L9Vc0443fop+UbFCabF0NWM6rJ31Nlv7s3mQIA
+-----END CERTIFICATE REQUEST-----'''
 
         result = self.runner.invoke(certificate.create,
-                                    ['--csr', 'dummy_csr',
+                                    ['--csr', csr,
                                      '--duration', 5,
-                                     '--package', 'cert_std_5_0_0'
+                                     '--max-altname', '5',
                                      ])
 
-        self.assertEqual(result.output, '')
+        wanted = '''The certificate create operation is 1
+You can follow it with:
+$ gandi certificate follow 1
+When the operation is DONE, you can retrieve the .crt with:
+$ gandi certificate export "domain.tld"
+'''
+
+        self.assertEqual(result.output, wanted)
         self.assertEqual(result.exit_code, 0)
