284b152f0bf15860973adc62297ee4a82d652e57
sayoun <sayoun@gandi.net>
2015-01-23 18:44:53 +0100

    Running Unittests must not erase configuration file
    Add a mock for Configuration save method, otherwise when someone
    runs unittests the real configuration in ~/.config/gandi was deleted
    and replace with unittest mocked values.

--- gandi/cli/tests/commands/base.py
+++ gandi/cli/tests/commands/base.py
@@ -1,14 +1,22 @@
 from click.testing import CliRunner
 
 from gandi.cli.core.base import GandiModule
-from ..compat import unittest
+from ..compat import unittest, mock
 from ..fixtures.api import Api
+from ..fixtures.mocks import MockObject
 
 
 class CommandTestCase(unittest.TestCase):
 
+    mocks = [('gandi.cli.core.base.GandiModule.save', MockObject.blank_func)]
+
     def setUp(self):
         self.runner = CliRunner()
+
+        self.mocks = [mock.patch(*mock_args) for mock_args in self.mocks]
+        for dummy in self.mocks:
+            dummy.start()
+
         GandiModule._api = Api()
         GandiModule._conffiles = {'global': {'api': {'env': 'test',
                                                      'key': 'apikey0001'}}}
@@ -16,3 +24,6 @@ class CommandTestCase(unittest.TestCase):
     def tearDown(self):
         GandiModule._api = None
         GandiModule._conffiles = {}
+
+        for dummy in reversed(self.mocks):
+            dummy.stop()
