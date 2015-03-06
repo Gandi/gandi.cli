c8efae00c2c9fdb91781d29030f26e4236ddac17
sayoun <sayoun@gandi.net>
2015-03-06 10:29:42 +0100

    Add method to retrieve events to module status with optionnal filters

--- gandi/cli/modules/status.py
+++ gandi/cli/modules/status.py
@@ -1,5 +1,6 @@
 """ Status commands module. """
 
+from datetime import datetime
 from gandi.cli.core.base import GandiModule
 
 
@@ -24,3 +25,24 @@ class Status(GandiModule):
     def services(cls):
         """Retrieve services statuses for status.gandi.net."""
         return cls.json_call('%s/services' % cls.base_url)
+
+    @classmethod
+    def events(cls, filters):
+        """Retrieve events details for status.gandi.net."""
+        current = filters.pop('current', False)
+        filter_url = '&'.join(['%s=%s' % (key, val)
+                               for key, val in filters.iteritems()])
+        events = cls.json_call('%s/events?%s' % (cls.base_url, filter_url))
+        now = datetime.utcnow()
+        dtformat = '%Y-%m-%dT%H:%M:%S'
+        current_events = []
+        for event in events:
+            if not current:
+                current_events.append(event)
+                continue
+            # don't parse the timezone without dateutil
+            ev_start = datetime.strptime(event['date_start'][:-6], dtformat)
+            if ev_start < now and not event['date_end']:
+                current_events.append(event)
+
+        return events
