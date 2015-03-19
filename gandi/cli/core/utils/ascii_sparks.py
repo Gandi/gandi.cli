f3be9ea2e208c3450209c035d1fefe639d1ac029
sayoun <sayoun@gandi.net>
2015-03-19 15:57:56 +0100

    Apply PEP8

--- gandi/cli/core/utils/ascii_sparks.py
+++ gandi/cli/core/utils/ascii_sparks.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 
-# Author: Rory McCann 
+# Author: Rory McCann
 # https://pypi.python.org/pypi/ascii_sparks/0.0.3
 # License: Unknown
 
 parts = u' ▁▂▃▄▅▆▇▉'
 
+
 def sparks(nums):
     fraction = max(nums) / float(len(parts) - 1)
-    return ''.join(parts[int(round(x/fraction))] for x in nums)
+    return ''.join([parts[int(round(x / fraction))] for x in nums])
