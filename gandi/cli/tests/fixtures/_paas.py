ef11b1df3963ad372552db2f82c1e2f47c033d2b
sayoun <sayoun@gandi.net>
2015-07-10 19:27:41 +0200

    Add missing paas fixture to tests

---
+++ gandi/cli/tests/fixtures/_paas.py
@@ -0,0 +1,10 @@
+
+def snapshotprofile_list(options):
+    ret = [{'id': 7,
+            'kept_total': 3,
+            'name': 'paas_normal',
+            'quota_factor': 1.3,
+            'schedules': [{'kept_version': 1, 'name': 'daily'},
+                          {'kept_version': 1, 'name': 'weekly'},
+                          {'kept_version': 1, 'name': 'weekly4'}]}]
+    return ret
