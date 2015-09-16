763af72a351ac098d4dfdfe04f4dc1a9b8ce7b9c
sayoun <sayoun@gandi.net>
2015-09-16 18:07:56 +0200

    Add unittests for oper commands

---
+++ gandi/cli/tests/commands/test_oper.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from .base import CommandTestCase
+from gandi.cli.commands import oper
+
+
+class OperTestCase(CommandTestCase):
+
+    def test_list(self):
+
+        result = self.invoke_with_exceptions(oper.list, [])
+
+        self.assertEqual(result.output, """\
+id        : 100200
+type      : billing_prepaid_add_money
+step      : BILL
+----------
+id        : 100100
+type      : domain_renew
+step      : BILL
+""")
+        self.assertEqual(result.exit_code, 0)
+
+    def test_info(self):
+
+        result = self.invoke_with_exceptions(oper.info, ['100100'])
+
+        self.assertEqual(result.output, """\
+id        : 100100
+type      : domain_renew
+step      : BILL
+last_error:
+""")
+        self.assertEqual(result.exit_code, 0)
--- gandi/cli/tests/fixtures/_operation.py
+++ gandi/cli/tests/fixtures/_operation.py
@@ -1,3 +1,65 @@
+try:
+    # python3
+    from xmlrpc.client import DateTime
+except ImportError:
+    # python2
+    from xmlrpclib import DateTime
+
+
+def list(options):
+
+    return [{'date_created': DateTime('20150915T18:29:16'),
+             'date_start': None,
+             'date_updated': DateTime('20150915T18:29:17'),
+             'errortype': None,
+             'eta': -1863666,
+             'id': 100100,
+             'infos': {'extras': {},
+                       'id': '',
+                       'label': 'iheartcli.com',
+                       'product_action': 'renew',
+                       'product_name': 'com',
+                       'product_type': 'domain',
+                       'quantity': ''},
+             'last_error': None,
+             'params': {'auth_id': 99999999,
+                        'current_year': 2015,
+                        'domain': 'iheartcli.com',
+                        'domain_id': 1234567,
+                        'duration': 1,
+                        'param_type': 'domain',
+                        'remote_addr': '127.0.0.1',
+                        'session_id': 2920674,
+                        'tld': 'com',
+                        'tracker_id': '621cb9f4-472d-4cc1-b4b9-b18cc61e2914'},
+             'session_id': 2920674,
+             'source': 'PXP561-GANDI',
+             'step': 'BILL',
+             'type': 'domain_renew'},
+            {'date_created': DateTime('20150505T00:00:00'),
+             'date_start': None,
+             'date_updated': DateTime('20150505T00:00:00'),
+             'errortype': None,
+             'eta': 0,
+             'id': 100200,
+             'infos': {'extras': {},
+                       'id': '',
+                       'label': '',
+                       'product_action': 'billing_prepaid_add_money',
+                       'product_name': '',
+                       'product_type': 'corporate',
+                       'quantity': ''},
+             'last_error': None,
+             'params': {'amount': 50.0,
+                        'auth_id': 99999999,
+                        'param_type': 'prepaid_add_money',
+                        'prepaid_id': 100000,
+                        'remote_addr': '127.0.0.1',
+                        'tracker_id': 'ab0e5e67-6ca7-4afc-8311-f20080f15cf1'},
+             'session_id': 9844958,
+             'source': 'PXP561-GANDI',
+             'step': 'BILL',
+             'type': 'billing_prepaid_add_money'}]
 
 
 def info(id):
@@ -10,3 +72,5 @@ def info(id):
 
     if id == 400:
         return {'step': 'DONE'}
+
+    return [oper for oper in list({}) if oper['id'] == id][0]
